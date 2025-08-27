021450e883632fcf2278c8680b383d98b296c4f4
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-08-27 14:43:32 -0300

    Realizado teste de unidade para classe de serviço do modulo GeneroFilme - "Cadastrar_Deve_RetornarOk_QuandoGeneroFilmeForValido".

--- ControleDeCinema.Testes.Unidade/ModuloGeneroFIlme/GeneroFilmeAppServiceTests.cs
+++ ControleDeCinema.Testes.Unidade/ModuloGeneroFIlme/GeneroFilmeAppServiceTests.cs
@@ -1,12 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using ControledeCinema.Dominio.Compartilhado;
+using ControleDeCinema.Aplicacao.ModuloGeneroFilme;
+using ControleDeCinema.Dominio.ModuloAutenticacao;
+using ControleDeCinema.Dominio.ModuloFilme;
+using ControleDeCinema.Dominio.ModuloGeneroFilme;
+using Microsoft.Extensions.Logging;
+using Moq;
 
-namespace ControleDeCinema.Testes.Unidade.ModuloGeneroFIlme
+namespace ControleDeCinema.Testes.Unidade.ModuloGeneroFilme;
+
+[TestClass]
+[TestCategory("Testes de unidade de genero filme")]
+
+public sealed class GeneroFilmeAppServiceTests
 {
-    internal class GeneroFilmeAppServiceTests
+
+    private Mock<IRepositorioGeneroFilme>? repositorioGeneroFilmeMock;
+    private Mock<IUnitOfWork>? unitOfWorkMock;
+    private Mock<ILogger<GeneroFilmeAppService>>? loggerMock;
+    private Mock<ITenantProvider>? tenantProviderMock;
+
+    private GeneroFilmeAppService? generoFilmeAppService;
+
+
+    [TestInitialize]
+    public void Setup()
     {
+        repositorioGeneroFilmeMock = new Mock<IRepositorioGeneroFilme>();
+        unitOfWorkMock = new Mock<IUnitOfWork>();
+        loggerMock = new Mock<ILogger<GeneroFilmeAppService>>();
+        tenantProviderMock = new Mock<ITenantProvider>();
+
+         generoFilmeAppService = new GeneroFilmeAppService(
+            tenantProviderMock.Object,
+            repositorioGeneroFilmeMock.Object,
+            unitOfWorkMock.Object,
+            loggerMock.Object
+        );
     }
+
+
+    [TestMethod]
+    public void Cadastrar_DeveRetornarOk_QuandoGeneroFilmeForValido()
+    {
+        // Arrange
+
+        var generoFilme = new GeneroFilme("Ação");
+        var generoFilmeTeste = new GeneroFilme("Teste");
+
+        repositorioGeneroFilmeMock?
+            .Setup(r => r.SelecionarRegistros())
+            .Returns(new List<GeneroFilme>() { generoFilmeTeste });
+        //Act
+
+        var resultado = generoFilmeAppService?.Cadastrar(generoFilme);
+
+        //Assert
+        repositorioGeneroFilmeMock?.Verify(r => r.Cadastrar(generoFilme), Times.Once);
+        unitOfWorkMock?.Verify(u => u.Commit(), Times.Once);
+
+        Assert.IsNotNull(resultado);
+        Assert.IsTrue(resultado.IsSuccess);
+    }
+
+
+
 }
+
+
