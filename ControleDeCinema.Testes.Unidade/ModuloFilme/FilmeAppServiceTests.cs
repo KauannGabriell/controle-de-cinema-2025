2a095fe3fceac71a6b7f6c70346b9f029a124636
coelho-glith <coelho.soldado7@gmail.com>
2025-08-29 15:12:35 -0300

    Realizado teste de unidade da camada de aplicação - Filme "Cadastrar_DeveRetornarOk_QuandoFilmeForValido"

--- ControleDeCinema.Testes.Unidade/ModuloFilme/FilmeAppServiceTests.cs
+++ ControleDeCinema.Testes.Unidade/ModuloFilme/FilmeAppServiceTests.cs
@@ -1,12 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ControledeCinema.Dominio.Compartilhado;
+using ControleDeCinema.Aplicacao.ModuloFilme;
+using Microsoft.Extensions.Logging;
+using ControleDeCinema.Dominio.ModuloAutenticacao;
+using ControleDeCinema.Dominio.ModuloFilme;
+using ControleDeCinema.Dominio.ModuloGeneroFilme;
+using Moq;
 
-namespace ControleDeCinema.Testes.Unidade.ModuloFilme
+
+namespace ControleDeCinema.Testes.Unidade.ModuloFilme;
+
+[TestClass]
+[TestCategory("Testes de unidade de filme")]
+
+public sealed class FilmeAppServiceTests
 {
-    internal class FilmeAppServiceTests
+
+    private Mock<IRepositorioFilme>? repositorioFilmeMock;
+    private Mock<IUnitOfWork>? unitOfWorkMock;
+    private Mock<ILogger<FilmeAppService>>? loggerMock;
+    private Mock<ITenantProvider>? tenantProviderMock;
+
+    private FilmeAppService? filmeAppService;
+
+
+    [TestInitialize]
+    public void Setup()
     {
+        repositorioFilmeMock = new Mock<IRepositorioFilme>();
+        unitOfWorkMock = new Mock<IUnitOfWork>();
+        loggerMock = new Mock<ILogger<FilmeAppService>>();
+        tenantProviderMock = new Mock<ITenantProvider>();
+
+        filmeAppService = new FilmeAppService(
+           tenantProviderMock.Object,
+           repositorioFilmeMock.Object,
+           unitOfWorkMock.Object,
+           loggerMock.Object
+       );
+    }
+
+
+    [TestMethod]
+    public void Cadastrar_DeveRetornarOk_QuandoFilmeForValido()
+    {
+        // Arrange
+
+        var generoFilme = new GeneroFilme("Ação");
+        var filme = new Filme("Titanic", 120, false, generoFilme);
+        var filmeTeste = new Filme("Teste", 70, false, generoFilme);
+
+        repositorioFilmeMock?
+            .Setup(r => r.SelecionarRegistros())
+            .Returns(new List<Filme>() { filmeTeste });
+        //Act
+        var resultado = filmeAppService?.Cadastrar(filme);
+
+        //Assert
+        repositorioFilmeMock?.Verify(r => r.Cadastrar(filme), Times.Once);
+        unitOfWorkMock?.Verify(u => u.Commit(), Times.Once);
+
+        Assert.IsNotNull(resultado);
+        Assert.IsTrue(resultado.IsSuccess);
     }
 }
